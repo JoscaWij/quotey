4104ee8938ba7ba8ffb32dcad5d8bc93850608ee
JoscaWij <Josca.W@gmx.net>
2020-08-28 10:41:57 +0200

    Create basic form and button

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,25 @@
 import React from "react";
+import styled from "@emotion/styled";
 
-function Form(props) {
-  return <div></div>;
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+function Form() {
+  return (
+    <>
+      <StyledForm>
+        <input placeholder="Enter Quote"></input>
+        <input placeholder="Enter author"></input>
+        <button type="submit">Submit</button>
+      </StyledForm>
+      <section>
+        <button>Get Quote</button>
+        <span>HERE QUOTY YOU HOPEFULLY WILL SEE</span>
+      </section>
+    </>
+  );
 }
 
 export default Form;
