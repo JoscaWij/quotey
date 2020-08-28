7abfee5249beddbdc62d8c3681fdb242db8d7385
JoscaWij <Josca.W@gmx.net>
2020-08-28 12:09:52 +0200

    Prepare mongoDB in server

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const { fetchQuotes } = require("./src/api/quotes");
+const { MongoClient } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = 3004;
 
+const client = new MongoClient(process.env.MONGO_URI);
+
 async function main() {
+  await client.connect();
+
+  const database = client.db(process.env.MONGO_DB_NAME);
+  const collection = database.collection("quotes");
+
   app.get("/quotes", async (request, response) => {
     const quotes = await fetchQuotes();
     response.send(quotes);
