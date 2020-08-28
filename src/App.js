419accc55d493f6cbd679419c7354d1dd7a98cf1
JoscaWij <Josca.W@gmx.net>
2020-08-28 15:31:18 +0200

    Add List and fetch in app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import Form from "./components/Form";
+import { fetchQuotes } from "./api/quotesDatabase";
+import List from "./components/List";
 
 function App() {
+  const [quotes, setQuotes] = React.useState("");
+
+  async function handleClick() {
+    const allQuotes = await fetchQuotes();
+    setQuotes(allQuotes);
+  }
+
   return (
     <div>
       <Form />
       <section>
-        <button>Get Quote</button>
-        <span>HERE QUOTY YOU HOPEFULLY WILL SEE</span>
+        <button onClick={() => handleClick()}>Get Quote</button>
+        <List quotes={quotes}></List>
       </section>
     </div>
   );
